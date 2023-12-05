5674f3cd8fd45858101591d8562ff7d1f19822c8
Bryan Brenes <bryanbrenes1@gmail.com>
2023-12-04 22:20:59 -0600

    Add second part of the day one puzzle

--- day-1/rust/main.rs
+++ day-1/rust/main.rs
@@ -1,37 +1,62 @@
 use std::fs;
-use std::time::SystemTime;
+
+#[derive(Debug)]
+struct NumberMatch {
+    name: String,
+    num: u8,
+}
+
 
 fn main() {
+    let number_names_table: [NumberMatch; 10] = [
+        NumberMatch {name: String::from("zero"), num: 0},
+        NumberMatch {name: String::from("one"), num: 1},
+        NumberMatch {name: String::from("two"), num: 2},
+        NumberMatch {name: String::from("three"), num: 3},
+        NumberMatch {name: String::from("four"), num: 4},
+        NumberMatch {name: String::from("five"), num: 5},
+        NumberMatch {name: String::from("six"), num: 6},
+        NumberMatch {name: String::from("seven"), num: 7},
+        NumberMatch {name: String::from("eight"), num: 8},
+        NumberMatch {name: String::from("nine"), num: 9},
+    ];
+
     let input = fs::read_to_string("../input-file.txt").expect("Cannot open file");
-    let init_time = SystemTime::now();
-    let mut sum: i32 = 0;
+    let lines = input.split('\n');
+    let mut sum: u32 = 0;
     let mut left_number: u8 = 255;
     let mut right_number: u8 = 255;
 
-    for byte in input.bytes() {
-        if byte != 10 {
-            if left_number != 255 && is_number(byte) {
-                right_number = byte - 48;
+    for line in lines {
+        if line.len() == 0 { continue }
+
+        for (i, char) in line.chars().enumerate() {
+            let slice = &line[i..line.len()];
+            for possible_match in &number_names_table {
+                if left_number == 255 && slice.starts_with(&possible_match.name){
+                    left_number = possible_match.num;
+                }
+
+                if left_number != 255 && slice.starts_with(&possible_match.name) {
+                    right_number = possible_match.num;
+                }
             }
 
-            if left_number == 255 && is_number(byte) {
-                left_number = byte - 48;
+            if left_number != 255 && char.is_digit(10) {
+                right_number = char.to_digit(10).expect("Cannot convert to u8") as u8;
             }
-        } else {
-            sum += (left_number * 10) as i32;
-            sum += (if right_number != 255 { right_number } else { left_number }) as i32;
-            right_number = 255;
-            left_number = 255;
+
+            if left_number == 255 && char.is_digit(10) {
+                left_number = char.to_digit(10).expect("Cannot convert to u8") as u8;
+            }
         }
+
+        sum += (left_number * 10) as u32;
+        sum += (if right_number != 255 { right_number } else { left_number }) as u32;
+        right_number = 255;
+        left_number = 255;
     }
 
     println!("Code: {sum}");
-    match init_time.elapsed() {
-        Ok(elapsed) => println!("Time: {}", elapsed.as_secs_f32()),
-        Err(e) => println!("Cannot get time elapsed {e}"),
-    }
 }
 
-fn is_number(byte: u8) -> bool {
-    return byte >= 48 && byte <= 57;
-}
