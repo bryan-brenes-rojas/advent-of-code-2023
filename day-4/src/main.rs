5adda498682dd2f5d3a70bc264699fb326c51314
Bryan Brenes <bryanbrenes1@gmail.com>
2023-12-07 22:53:59 -0600

    Day 4: completed part 1

--- day-4/src/main.rs
+++ day-4/src/main.rs
@@ -1,20 +1,28 @@
-use std::fs;
+use std::{fs, collections::HashSet};
+
+type Card = (HashSet<u8>, HashSet<u8>);
 
 fn main() {
-    let input = fs::read_to_string("assets/test-input.txt").unwrap();
+    let input = fs::read_to_string("assets/input.txt").unwrap();
     let cards = process_input(&input);
-    for card in cards {
-        println!("{:?}", card);
+    let mut sum = 0;
+    for (winning_numbers, numbers_gotten) in cards {
+        let intersection = winning_numbers.intersection(&numbers_gotten);
+        let size = &intersection.count();
+        if *size > 0 {
+            sum += 2u32.pow((*size - 1) as u32);
+        }
     }
+    println!("Sum: {sum}");
 }
 
-fn process_input(input: &str) -> Vec<(Vec<u8>, Vec<u8>)> {
+fn process_input(input: &str) -> Vec<Card> {
     let mut semi_colon_found = false;
     let mut pipe_found = false;
     let mut number_buffer = String::from("");
-    let mut cards: Vec<(Vec<u8>, Vec<u8>)> = vec![];
-    let mut winning_numbers: Vec<u8> = vec![];
-    let mut number_gotten: Vec<u8> = vec![];
+    let mut cards: Vec<Card> = vec![];
+    let mut winning_numbers: HashSet<u8> = HashSet::new();
+    let mut number_gotten: HashSet<u8> = HashSet::new();
 
     for char in input.chars() {
         if char == ':' { semi_colon_found = true }
@@ -25,13 +33,13 @@ fn process_input(input: &str) -> Vec<(Vec<u8>, Vec<u8>)> {
             number_buffer = format!("{number_buffer}{char}");
         } else if char == ' ' && !number_buffer.is_empty() {
             if pipe_found {
-                number_gotten.push(number_buffer.parse().unwrap());
+                number_gotten.insert(number_buffer.parse().unwrap());
             } else {
-                winning_numbers.push(number_buffer.parse().unwrap());
+                winning_numbers.insert(number_buffer.parse().unwrap());
             }
             number_buffer.clear();
         } else if char == '\n' {
-            number_gotten.push(number_buffer.parse().unwrap());
+            number_gotten.insert(number_buffer.parse().unwrap());
             cards.push((winning_numbers.clone(), number_gotten.clone()));
             number_buffer.clear();
             winning_numbers.clear();
